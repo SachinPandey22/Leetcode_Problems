6b0ffbd9e1fbdd735d3ba551206d8841102c956e
Sachin Pandey <152921607+SachinPandey22@users.noreply.github.com>
2026-09-09 14:01:16 -0500

    Time: 3 ms (89.24%), Space: 19.3 MB (16.74%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,12 +1,12 @@
 class Solution:
+    import math
     def checkPerfectNumber(self, num: int) -> bool:
         sum = 0
-        if num == 1:
+        if num <= 1 or num > 10**8:
             return False
-        for i in range(1, int(math.sqrt(num))+1, 1):
-            if num % i == 0:
-                sum = sum + i
-                if i != num/i and num/i != num: 
-                    sum = sum + num/i
-        return num == sum
-        
+        for n in range(1, int(math.sqrt(num)) +1 , 1):
+            if num%n ==0:
+                sum += n
+                if n!=num/n and num/n !=num:
+                    sum += num/n
+        return sum == num
